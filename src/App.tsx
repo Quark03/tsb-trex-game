67182e0ae58381354584071db264128ad7bdd715
António Festas <antonio.c.festas@gmail.com>
2024-11-07 18:50:45 +0000

    Changed page background

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import Game from "./Game"
 
 function App() {
   return (
-    <main className="h-screen w-screen relative flex flex-col items-center justify-center">
-      <div className="flex items-center gap-4 mb-6">
-        <img className="h-16" src="/TSB_Logo.png" alt="TSB Logo" />
+    <main className="h-screen w-screen relative flex flex-col items-center justify-center bg-blue-50">
+      <div className="flex items-center flex-col gap-4 mb-6">
+        <img className="h-24" src="/TSB_Logo.png" alt="TSB Logo" />
         <h1 className="text-4xl font-ethnocentric uppercase">Técnico <span className="text-tsb-blue">Solar</span> Boat</h1>
       </div>
       <Game />
